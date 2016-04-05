aea3ac69b19d393f4d514362fb3531eb01c97ee2
游峰 <youfeng@youfengdeMacBook-Pro.local>
2016-04-05 19:38:29 +0800

    init

---
+++ iPadDemos/iPadDemos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPadDemos
+//
+//  Created by 游峰 on 16/4/5.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPadDemos/iPadDemos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPadDemos
+//
+//  Created by 游峰 on 16/4/5.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
