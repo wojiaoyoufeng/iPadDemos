91deb805f7f738b856b080c9d5cba18eb00a1474
游峰 <youfeng@youfengdeMacBook-Pro.local>
2016-04-27 06:07:19 +0800

    storyboard, 导入MapKit

---
+++ iPadDemos/MapKit/MainMapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainMapViewController.h
+//  iPadDemos
+//
+//  Created by 游峰 on 16/4/25.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MainMapViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@end
---
+++ iPadDemos/MapKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapKit
+//
+//  Created by 游峰 on 16/4/25.
+//  Copyright © 2016年 yf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
